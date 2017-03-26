49c8ea382a8bd51731f3526d9474e9e1f56b5072
Tomas Nilsson <nilsson.tomas@gmail.com>
2017-03-26 22:19:46 +0200

    Completed exercise 4

---
+++ src/ex_004_palindrome_product.py
@@ -0,0 +1,46 @@
+""""" Largest palindrome product
+Problem 4
+A palindromic number reads the same both ways. The largest palindrome made
+from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+Answer: 906609
+For 2x2 -> the palindrome can be written abba.
+1000 a + 1a = 1001a
+100b + 10b = 110b
+Simplify to
+11x(91a + 10b)
+
+for 3x3 the math becomes (abccba)
+100001a + 10010b + 1100c
+11x (9091z, 910b, 100c)
+"""
+
+def find_palindrome_product(from_number, to_number):
+    """ "Find the largest palindrome number"""
+    largest_product = 0
+    # Ensure that the to_number is divisible by 11 according to above math
+    modified_to = 11 * int((to_number / 11))
+    for smallstep in range(to_number, from_number, -1):
+        for bigstep in range(modified_to, from_number, -11):
+            prod = bigstep * smallstep
+            if prod <= largest_product:
+                break
+            if is_palindrome(str(prod)):
+                largest_product = prod
+
+    return largest_product
+
+def is_palindrome(to_check):
+    """ Check if a string is a palindrome"""
+    return to_check == to_check[::-1]
+
+def run_main():
+    """ Run the exercise as stated on the Euler website and wrapped in a timer"""
+    from src.tools import timer_decorator
+    wrapped_function = timer_decorator.wrap(find_palindrome_product)
+    return wrapped_function(99, 999)
+
+if __name__ == "__main__":
+    run_main() # pragma: no cover
---
+++ tests/test_004_palindrome_product.py
@@ -0,0 +1,20 @@
+""" Tests for ex_001_multiples"""
+from nose2.tools import such
+import nose2
+from src import ex_004_palindrome_product as mut
+
+with such.A('Palindrome exercise') as it:
+    @it.should('Calculate a 2x2 max palindrome correctly')
+    def test_2_by_2():
+        """ Test the 2x2 case """
+        assert mut.find_palindrome_product(9, 99) == 9009
+    @it.should('Solve the exercise correctly')
+    def test_main():
+        """ Check that it solves the main problem correctly """
+        assert mut.run_main() == 906609
+
+
+it.createTests(globals())
+
+if __name__ == '__main__':
+    nose2.main() # pragma: no cover
